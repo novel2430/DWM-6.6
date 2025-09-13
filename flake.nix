3892c0feffbfe289038047a3556b837619ae15b7
novel2430 <novel2430@163.com>
2025-09-13 18:14:34 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "DWM Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+  
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    libs = with pkgs; with pkgs.xorg; [
+        libX11
+        libXinerama
+        libXft
+      ];
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      packages = [];
+      buildInputs = libs;
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+    };
+  };
+}
